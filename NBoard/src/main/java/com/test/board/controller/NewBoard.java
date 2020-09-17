42d002b431e40f5b23d62356a62e61bfbf7af61d
jinhong <jin.ryu0912@gmail.com>
2020-09-18 01:04:43 +0900

    깃사용하기 use

--- NBoard/src/main/java/com/test/board/controller/NewBoard.java
+++ NBoard/src/main/java/com/test/board/controller/NewBoard.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/admin")
 public class NewBoard {
 
 	
 	@RequestMapping("/home")
 	public String getHome() {
 		return "/testBoard/Newboard";
-	
+		
+		
 		
 		
 	}
