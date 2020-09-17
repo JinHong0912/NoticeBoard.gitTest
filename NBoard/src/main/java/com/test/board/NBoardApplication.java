f8c9b87eec8322dacf23e196b523f8e36c401b00
jinhong <jin.ryu0912@gmail.com>
2020-09-17 23:17:22 +0900

    board 만들기 및 깃헙 연

---
+++ NBoard/src/main/java/com/test/board/NBoardApplication.java
@@ -0,0 +1,13 @@
+package com.test.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NBoardApplication.class, args);
+	}
+
+}
