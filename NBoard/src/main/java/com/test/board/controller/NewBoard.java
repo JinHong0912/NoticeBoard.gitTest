a2fa9c40c157be5e9d133fcdf5c66cdd5d884d63
jinhong <jin.ryu0912@gmail.com>
2020-09-18 01:02:40 +0900

    두번째 연습

--- NBoard/src/main/java/com/test/board/controller/NewBoard.java
+++ NBoard/src/main/java/com/test/board/controller/NewBoard.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
+
 public class NewBoard {
 
 	
 	@RequestMapping("/home")
 	public String getHome() {
 		return "/testBoard/Newboard";
-		
 		
 		
 		
